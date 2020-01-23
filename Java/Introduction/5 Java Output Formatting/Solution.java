e0eb8241b49de1b9b267943859227b792bde9035
Amogha KS <43683444+AmoghKS@users.noreply.github.com>
2020-01-23 10:17:39 +0530

    cmt

--- Java/Introduction/5 Java Output Formatting/Solution.java
+++ Java/Introduction/5 Java Output Formatting/Solution.java
@@ -1,6 +1,8 @@
 // github.com/RodneyShag
 // ("%-15s%03d\n", s1, x) % : used as a formatter. '-' : minus sign used for left indentation of the string. 15s : here 15 denotes the string's minimum field width 15. '0' : pads the extra 0s in the integer. 3d : here 3 denotes integer's minimum field width 3. \n (%n can also be used) : prints the new line.
 
+//%-15s" means that within 15 blank space, the String "s1" will be filled in the left. (fill in the blanks from the left)
+
 import java.util.Scanner;
 
 public class Solution {
